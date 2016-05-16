af5aa179ebd96be469db894d29fae8bf4827b48f
Lucas Pereira de Lima <lucas01_ce@hotmail.com>
2016-05-16 17:46:13 -0300

    Commit após realização da última aula do modulo I do curso de JSF.

--- src/br/com/caelum/livraria/bean/LoginBean.java
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,9 +1,9 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
-import javax.faces.view.facelets.FaceletContext;
 
 import br.com.caelum.livraria.dao.UsuarioDao;
 import br.com.caelum.livraria.modelo.Usuario;
@@ -21,13 +21,26 @@ public class LoginBean {
 	public String efetuarLogin() {
 		System.out.println("fazendo login do usuário.");
 		boolean isExiste = new UsuarioDao().existeUsuario(this.usuario);
+		FacesContext context = FacesContext.getCurrentInstance();
 		if(isExiste)
 		{
-			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";
 		}
 		else
-			return "login";
+		{
+			context.getExternalContext().getFlash().setKeepMessages(true);
+			context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+			return "login?faces-redirect=true";
+		}
+	}
+	
+	public String deslogar() {
+
+	    FacesContext context = FacesContext.getCurrentInstance();
+	    context.getExternalContext().getSessionMap().remove("usuarioLogado");
+
+	    return "login?faces-redirect=true";
 	}
 	
 }
